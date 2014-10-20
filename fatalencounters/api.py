6245ec96899907a23a66a7e3a6016ec1bb8e294f
Josh Levinger <josh@levinger.net>
2014-10-20 11:26:24 -0700

    api filtering

--- fatalencounters/api.py
+++ fatalencounters/api.py
@@ -1,3 +1,4 @@
+from tastypie import fields, utils
 from tastypie.resources import ModelResource
 from fatalencounters.models import FatalEncounter
 
@@ -5,7 +6,15 @@ from fatalencounters.models import FatalEncounter
 class FatalEncounterResource(ModelResource):
 
     class Meta:
-        queryset = FatalEncounter.objects.all()
+        queryset = FatalEncounter.objects.filter(proofed=True)
         list_allowed_methods = ['get', ]
-        detail_allowed_methods = ['get']
+        detail_allowed_methods = ['get', ]
+        excludes = ['proofed', 'photo_url', 'source_url']
         resource_name = 'fatalencounters'
+
+        filtering = {
+            'date_of_injury': ['exact', 'lt', 'lte', 'gte', 'gt'],
+            'state': ['exact', ],
+            'city': ['exact', ],
+            'race': ['exact', ],
+        }
