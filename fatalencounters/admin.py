feb4ea11f2da27d6b4e48a28d83baca4bea5dc67
Josh Levinger <josh@levinger.net>
2015-03-13 19:51:13 -0700

    admin export

--- fatalencounters/admin.py
+++ fatalencounters/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from fatalencounters.models import FatalEncounter, County
 
+from admin_exporter.actions import export_as_csv_action
+
 class FatalEncounterAdmin(admin.ModelAdmin):
-  list_filter = ('gender', 'race', 'proofed')
+  list_filter = ('gender', 'race', 'state', 'proofed')
   list_display = ('name', 'age', 'city', 'county', 'state', 'race', 'date_of_injury', 'photo_tag')
   search_fields = ('name','city','county','state')
   date_hierarchy = 'date_of_injury'
@@ -17,6 +19,6 @@ class CountyAdmin(admin.ModelAdmin):
   list_filter = ('state',)
   search_fields = ('name',)
 
-
+admin.site.add_action(export_as_csv_action)
 admin.site.register(FatalEncounter, FatalEncounterAdmin)
 admin.site.register(County, CountyAdmin)
--- killedbycops/settings.py
+++ killedbycops/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 #    'compressor',
     'tastypie',
+    'admin_exporter',
     'fatalencounters',
     'tweets',
     'map',
