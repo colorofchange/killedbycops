84792fc3d91b29b23372c7798cc8ff8f7f69fe7d
Josh Levinger <josh@levinger.net>
2015-05-28 12:42:43 -0700

    show proof status in admin

--- fatalencounters/admin.py
+++ fatalencounters/admin.py
@@ -6,14 +6,15 @@ from admin_exporter.actions import export_as_csv_action
 
 class FatalEncounterAdmin(admin.ModelAdmin):
     list_filter = ('gender', 'race', 'state', 'proofed')
-    list_display = ('name', 'age', 'city', 'county', 'state', 'race', 'date_of_injury', 'photo_tag')
+    list_display = ('name', 'age', 'city', 'county', 'state', 'race', 'date_of_injury', 'photo_tag','proofed')
     search_fields = ('name', 'city', 'county', 'state')
     date_hierarchy = 'date_of_injury'
     fieldsets = [
         ('Biographical', {'fields': ['name', 'age', 'gender', 'race']}),
         ('Incident', {'fields': ['date_of_injury', 'city', 'county', 'state', 'agency_responsible']}),
-        ('Sources', {'fields': ['source_url', 'photo_url']}),
+        ('Sources', {'fields': ['source_url', 'photo_url','proofed']}),
     ]
+    readonly_fields = ('proofed',)
 
 
 admin.site.add_action(export_as_csv_action)
