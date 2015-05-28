bb1310df786fd1808ccac89130f667dfc80deff7
Josh Levinger <josh@levinger.net>
2015-05-28 12:42:33 -0700

    update_fatal_encounters use logger

--- fatalencounters/management/commands/update_fatal_encounters.py
+++ fatalencounters/management/commands/update_fatal_encounters.py
@@ -5,9 +5,14 @@ import urllib
 from datetime import datetime
 import csv
 import os, sys
+import logging
 
 from fatalencounters.models import FatalEncounter
 from data_corrections import corrections
+
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger('update_fatal_encounters')
+
 
 class Command(BaseCommand):
     args = '--pull_from_web'
@@ -24,13 +29,16 @@ class Command(BaseCommand):
             type="int",
             default=None,
             dest='last_proofed_row',
-            help='Last from the spreadsheet that was proofed by editors'),
+            help='Last row from the spreadsheet that was proofed by editors'),
+        make_option('--last_proofed_name',
+            action='store',
+            type="string",
+            default="Dixon Rodriguez",
+            dest='last_proofed_name',
+            help='Last name from the spreadsheet that was proofed by editors'),
         )
 
     def handle(self, *args, **options):
-        if not options['last_proofed_row']:
-            print "last_proofed_row option required. Open CSV and look for line # for Dixon Rodriguez. See README for justification."
-            sys.exit(-1)
 
         if options['pull_from_web']:
             spreadsheet_url = "https://docs.google.com/spreadsheet/pub?key=0Aul9Ys3cd80fdHNuRG5VeWpfbnU4eVdIWTU3Q0xwSEE&output=csv"
@@ -39,7 +47,10 @@ class Command(BaseCommand):
             print 'Updated data/fatal-encounters.csv'
         else:
             if os.path.exists('fatalencounters/data/fatal-encounters.csv'):
-                print 'Using existing data/fatal-encounters.csv from', os.path.getmtime('fatalencounters/data/fatal-encounters.csv')
+                mtime = os.path.getmtime('fatalencounters/data/fatal-encounters.csv')
+                mtimestamp = datetime.fromtimestamp(mtime)
+                print 'Using existing data/fatal-encounters.csv from',
+                print mtimestamp.strftime('%Y-%m-%d %H:%M:%S')
             else:
                 print 'No existing data/fatal-encounters.csv'
                 print 'Try again with --pull_from_web'
@@ -48,25 +59,40 @@ class Command(BaseCommand):
         with open('fatalencounters/data/fatal-encounters.csv') as csv_file:
             csv_reader = csv.reader(csv_file)
             header = csv_reader.next()
-            print 'Header:', header
+            logger.info('Header:%s' % header)
 
             row_num = 0
+            last_proofed_row = options['last_proofed_row']
+            if not last_proofed_row:
+                logger.warning("no last_proofed_row specified, looking for %s " % options['last_proofed_name'])
+            num_created, num_existing, num_corrected, num_unproofed = 0, 0, 0, 0
+
             for row in csv_reader:
                 row_num += 1
-                
                 try:
                     data = {'name':row[1].strip(), 'age':row[2],
                             'city':row[8], 'state':row[9][:2], 'agency_responsible':row[12],
                             'photo_url': row[5], 'source_url':row[16]}
 
-                    if options['last_proofed_row'] and row_num <= options['last_proofed_row']:
-                        data['proofed'] = True
+                    # check proof status
+                    if last_proofed_row:
+                        if row_num <= last_proofed_row:
+                            data['proofed'] = True
+                        else:
+                            num_unproofed += 1
+                            data['proofed'] = False
                     else:
-                        data['proofed'] = False
+                        # in not specifying last_proofed_row, assume that rows are proofed until we hit flag
+                        data['proofed'] = True
+
+                    # AFAIK they are putting un-proofed data after Dixon Rodriguez entry
+                    if not last_proofed_row and (data['name'] == options['last_proofed_name']):
+                        logger.warning("got %s, last_proofed_row=%d" % (options['last_proofed_name'], row_num))
+                        last_proofed_row = row_num
 
                     #convert Male/Female to single character
                     if len(row[3]) > 1:
-                        data['gender'] =  row[3][0]
+                        data['gender'] = row[3][0]
 
                     #deal with un-structured races
                     race_us = row[4].strip()
@@ -115,11 +141,11 @@ class Command(BaseCommand):
                         if date.year < 1900:
                             #assume it's actually in 20xx
                             corrected_year = int('20' + str(date.year)[-2:])
-                            print 'fixing year for',row[6],'to',corrected_year
+                            logger.warning('fixing year "%s" for %s to %s' % (fe.year, fe.name, corrected_year))
                             data['date_of_injury'] = date.replace(year=corrected_year)
                         if date.year > datetime.today().year:
                             this_year = datetime.today().year
-                            print 'fixing year for',row[6],'to',this_year
+                            logger.warning('fixing year "%s" for %s to %s' % (fe.year, fe.name, this_year))
                             data['date_of_injury'] = date.replace(year=this_year)
 
                     #filter non-numeric ages
@@ -136,20 +162,35 @@ class Command(BaseCommand):
                             defaults=data)
 
                     if created:
-                        print 'row',row_num,'created',fe.name,':', fe.race
+                        num_created += 1
+                        logger.info('row %d created %s:%s' % (row_num, fe.name, fe.race))
                     else:
-                        print 'row',row_num,'updated',fe.name,':', fe.race
+                        num_existing += 1
+                        logger.info('row %d exists %s:%s' % (row_num, fe.name, fe.race))
+                    #unfortunately no way to tell from update_or_create which fields were updated
 
+                    #apply corrections for known typos
                     if fe.name in corrections:
-                        print 'corrected',
+                        corrected_fields = False
                         for (key,value) in corrections[fe.name].items():
-                            setattr(fe,key,value) #because django models won't do obj[key] = value
-                            print '%s to %s' % (key,value),
+                            existing_value = getattr(fe,key)
+                            if existing_value != value:
+                                setattr(fe,key,value) #because django models won't do obj[key] = value
+                                corrected_fields = True
+                                logger.warning('corrected %s %s %s to %s' % (fe.name,key,existing_value,value))
+                        if corrected_fields:
+                            num_corrected += 1
                         fe.save()
-                        print
 
                 except Exception, e:
-                    print 'error saving #',row_num
-                    print e
-                    print row
+                    logger.error('error saving #%s' % row_num)
+                    logger.error(e)
+                    logger.error(row)
                     continue
+
+            print "FatalEncounters update complete"
+            print num_created, "records created"
+            print num_existing, "records existing"
+            print num_corrected, "records corrected"
+            print num_unproofed, "records not proofed"
+
